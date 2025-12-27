822ac51cab62a5c0bead3bf9ee172af98aec968c
Mullaguru <2463479@cognizant.com>
2025-12-27 20:56:38 +0530

    Initial backend commit

---
+++ ecommerce-backend/src/main/java/com/ecommerce/order/OrderController.java
@@ -0,0 +1,5 @@
+package com.ecommerce.order;
+
+public class OrderController {
+
+}
---
+++ ecommerce-backend/src/main/java/com/ecommerce/product/ProductController.java
@@ -0,0 +1,10 @@
+package com.ecommerce.product;
+
+public class ProductController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
